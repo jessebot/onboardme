68e68ed6eab33a7dc595abec8169d47e8a227850
JesseBot <jessebot@linux.com>
2022-06-29 13:24:18 +0200

    adding logic to use different firefox dirs for different os

--- configs/browser/firefox/grab_all_firefox_extensions.py
+++ configs/browser/firefox/grab_all_firefox_extensions.py
@@ -1,23 +1,39 @@
 #!/usr/bin/env python3
-# import and print all the firefox add-on ids from macOS
+# import and print all the macos firefox add-on ids in mardown table
+# and then moves all the xpis to a local directory: distributions/extensions
 import json
 import shutil
+from sys import platform
 
 USER_NAME='jhitch'
 FIREFOX_PROFILE="n390gdog.default-release"
-EXTENSION_FILE_PATH=(f"/Users/{USER_NAME}/Library/Application Support/Firefox/"
-                     f"Profiles/{FIREFOX_PROFILE}/extensions.json")
 
-print(f'Processing add-ons from:\n{EXTENSION_FILE_PATH}')
+# different os will have this in different places
+if platform == "linux" or platform == "linux2":
+    # linux - untested
+    PROFILE_PATH=(f"/home/{USER_NAME}/Firefox/Profiles/{FIREFOX_PROFILE}/")
+elif platform == "darwin":
+    # OS X
+    PROFILE_PATH=(f"/Users/{USER_NAME}/Library/Application Support/Firefox/"
+                  f"Profiles/{FIREFOX_PROFILE}/")
+elif platform == "win32" or platform == "windows":
+    # Windows... - untested
+    PROFILE_PATH=(f"/Users/{USER_NAME}/Library/Application Support/Firefox/"
+                  f"Profiles/{FIREFOX_PROFILE}/")
 
-# load the json into a python dict
+EXT_JSON_FILE = PROFILE_PATH + "extensions.json" 
+
+print(f'Processing add-ons from:\n{EXT_JSON_FILE}')
+
+# load the extensions.json into a python dict
 extensions_json = None
-with open(EXTENSION_FILE_PATH) as json_file:
+with open(EXT_JSON_FILE) as json_file:
     data = json_file.read()
     extensions_json = json.loads(data)
 
-# print each extension's add-on ID, as well as the human readable name
-print("|   add-on name   |   add-on ID   |")
+print("add-on name | add-on ID")
+print("---|---")
+# iterate through the new json dict and of add-ons and grab the name and id
 for add_on_dict in extensions_json["addons"]:
     add_on_id = add_on_dict["id"]
     add_on_name = add_on_dict["defaultLocale"]["name"]
@@ -42,11 +58,12 @@ for add_on_dict in extensions_json["addons"]:
                           "doh-rollout@mozilla.org"]
 
     if not any(add_on_id in s for s in ignored_add_on_ids):
+        # print each extension's name and add-on ID
         print(f"| {add_on_name} | {add_on_id} |")
-        extensions_dir = EXTENSION_FILE_PATH.replace(".json","")
-        extensions_xpi_file = f"{add_on_id}.xpi"
-        extensions_xpi_file_path = "/".join([extensions_dir,
-                                             extensions_xpi_file])
 
-        shutil.copyfile(extensions_xpi_file_path,
+        # also copy extension xpi files locally
+        extensions_xpi_file = f"{add_on_id}.xpi"
+        extensions_dir = PROFILE_PATH + "extensions/"  + extensions_xpi_file
+
+        shutil.copyfile(extensions_dir,
                         f"./distribution/extensions/{extensions_xpi_file}")
