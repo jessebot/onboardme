fd4cf421ea46b4cb31b892f82dc1988d9eb17482
JesseBot <jessebot@linux.com>
2022-07-01 11:16:38 +0200

    fixing how output works

--- setup.sh
+++ setup.sh
@@ -2,54 +2,66 @@
 # just a quick script to install some reqs, works on debian based distros and macOS
 # Checks for: Brew, git, python3, and python3 requirements.txt
 
+echo "-------------------------------- Beginning Setup -------------------------------"
 # git should be default installed on macOS Monterey, we but should check linux
 which git > /dev/null
 git_return_code=$?
 if [ $git_return_code -ne 0 ]; then
-    printf "Git not installed or in path, attempting to install git...\n\n"
+    echo "Git not installed or in path, attempting to install git..."
     if [ $(uname) == "linux" ]; then
         sudo apt install git
     fi
 else
-    printf "Git already installed :>\n\n"
+    echo "Git already installed :>"
+fi
+echo "--------------------------------------------------------------------------------"
+
+# make sure linuxbrew is in the path
+if [ $(uname) == "linux" ]; then
+    export PATH=$PATH:/home/linuxbrew/.linuxbrew/bin
 fi
 
 # check to make sure we have brew installed
 which brew > /dev/null
 brew_return_code=$?
 if [ $brew_return_code -ne 0 ]; then
-    printf "installing brew really quick, this will require your credentials for sudo abilities...\n\n"
+    echo "Installing brew really quick, this will require your credentials for sudo abilities..."
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 else
-    printf "Brew is already installed :)\n\n"
+    echo "Brew is already installed :)"
 fi
+echo "--------------------------------------------------------------------------------"
 
 
 # check to make sure we have python installed
 which python3 > /dev/null
 py_return_code=$?
 if [ $py_return_code -ne 0 ]; then
-    printf "We don't have python3 installed, installing...\n\n"
+    echo "Installing Python3..."
     brew install python3
 else
-    printf "Python3 is already installed :D\n\n"
+    echo "Python3 is already installed :D"
 fi
+echo "--------------------------------------------------------------------------------"
 
 
 # I always put my projects in a directory called repos, idk why I can't stop...
-printf "Creating directory structure...\n\n"
+echo "Creating directory structure..."
 mkdir -p ~/repos
+echo "--------------------------------------------------------------------------------"
 
 # Let's just download the repo now 
 # haven't tested this: gh repo clone jessebot/onboardme
-printf "Cloning the onboardme repo into ~/repos/onboardme ...\n\n"
+echo "Cloning the onboardme repo into ~/repos/onboardme ..."
 git clone https://github.com/jessebot/onboardme.git ~/repos/onboardme
+echo "--------------------------------------------------------------------------------"
 
 
 # make sure we have wget and pyyaml
-printf "Now installing python reqs...\n\n"
-pip install -r ~/repos/onboardme/requirements.txt
-
-printf "Success~! You should be able to use onboardme.py now ^O^\n\n"
-printf "Try running the following:\n"
-printf "~/repos/onboardme/onboardme.py\n\n"
+echo "Now installing python reqs..."
+pip install -r ~/repos/onboardme/requirements.txt 
+echo ""
+echo "------------------------------ Success~! ^O^ -----------------------------------"
+echo ""
+echo "Now, try running the following: ~/repos/onboardme/onboardme.py"
+echo ""
