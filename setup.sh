10b5693ce8a369ebc83a6fed2cc0f44c2ceca5cd
JesseBot <jessebot@linux.com>
2022-07-01 11:01:23 +0200

    removed needless cd, and clone directly into path

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 # Checks for: Brew, git, python3, and python3 requirements.txt
 
 # git should be default installed on macOS Monterey, we but should check linux
-which git
+which git > /dev/null
 git_return_code=$?
 if [ $git_return_code -ne 0 ]; then
     printf "Git not installed or in path, attempting to install git...\n\n"
@@ -15,7 +15,7 @@ else
 fi
 
 # check to make sure we have brew installed
-which brew
+which brew > /dev/null
 brew_return_code=$?
 if [ $brew_return_code -ne 0 ]; then
     printf "installing brew really quick, this will require your credentials for sudo abilities...\n\n"
@@ -26,7 +26,7 @@ fi
 
 
 # check to make sure we have python installed
-which python3
+which python3 > /dev/null
 py_return_code=$?
 if [ $py_return_code -ne 0 ]; then
     printf "We don't have python3 installed, installing...\n\n"
@@ -38,17 +38,17 @@ fi
 
 # I always put my projects in a directory called repos, idk why I can't stop...
 printf "Creating directory structure...\n\n"
-mkdir -p ~/repos && cd ~/repos
+mkdir -p ~/repos
 
 # Let's just download the repo now 
 # haven't tested this: gh repo clone jessebot/onboardme
 printf "Cloning the onboardme repo into ~/repos/onboardme ...\n\n"
-git clone https://github.com/jessebot/onboardme.git
+git clone https://github.com/jessebot/onboardme.git ~/repos/onboardme
 
 
 # make sure we have wget and pyyaml
 printf "Now installing python reqs...\n\n"
-pip install -r onboardme/requirements.txt
+pip install -r ~/repos/onboardme/requirements.txt
 
 printf "Success~! You should be able to use onboardme.py now ^O^\n\n"
 printf "Try running the following:\n"
