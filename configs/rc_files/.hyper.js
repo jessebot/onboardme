e79c8953c9ddb3f441e8ac0bd6290c03eb8892b8
JesseBot <jessebot@linux.com>
2022-07-03 11:41:24 +0200

    updating color for hyper for default configure to copy over

--- configs/rc_files/.hyper.js
+++ configs/rc_files/.hyper.js
@@ -31,7 +31,7 @@ module.exports = {
         foregroundColor: '#fff',
         // terminal background color
         // opacity is only supported on macOS
-        backgroundColor: '#000',
+        backgroundColor: '#161616',
         // terminal selection color
         selectionColor: 'rgba(248,28,229,0.3)',
         // border color (window, tabs)
