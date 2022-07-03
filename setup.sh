1fa5dce6f84401f694bf8dcc841049ffdee6d49a
JesseBot <jessebot@linux.com>
2022-07-03 21:29:25 +0200

    make setup cozier

--- setup.sh
+++ setup.sh
@@ -5,9 +5,23 @@
 # we make this lowercase because I don't trust it not to be random caps
 OS=$(uname | tr '[:upper:]' '[:lower:]')
 
+echo "Before we begin, please make sure you're on a wired connection, or sit "
+echo " close to the wifi. This might take a while if you're a fresh OS install"
+echo " and you'll need to present to enter your password for package installs"
+printf "so grab some tea and get comfy.\n\n"
+echo "Here's some relaxing music: https://youtu.be/-5KAN9_CzSA"
+while true; do
+    read -p "Are you ready to get started? Do you have a nice cup of ☕? [y/n]  " answer
+    if [ "$answer" != "y" ]; then
+        echo "That's totally reasonable. You take your time, and I will be here."
+        sleep 5
+    else
+        break
+    fi
+done
+
 echo -e "-------------------------------- \033[94m Beginning Setup \033[00m -------------------------------"
 echo "This script will ask for your password to sudo install things"
-echo "Before we begin, please make sure you're on a wired connection, or sit close to the wifi."
 echo ""
 if [[ "$OS" == *"linux"* ]]; then
     echo -e "---------------------------- \033[94m Updating existing apt packages \033[00m --------------------"
