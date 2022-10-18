a81bf8d443fa50a1c3b366afd6f94cfc7b89517f
JesseBot <jessebot@linux.com>
2022-10-18 21:36:27 +0200

    adding various params to setup.sh

--- setup.py
+++ setup.py
@@ -1,18 +1,35 @@
 from setuptools import setup
 
+
+def readme():
+    """
+    grab and return contents of README.md for use in long description
+    """
+    with open('README.md') as f:
+        return f.read()
+
+
 setup(name='onboardme',
-      version='0.1',
       description='An onboarding tool to install dot files and packages',
+      long_description=readme(),
+      classifiers=[
+          'Development Status :: 3 - Alpha'
+          'Programming Language :: Python :: 3.10'
+          'Topic :: Image Processing :: Display',
+      ],
+      classifiers=[
+          'Development Status :: 3 - Alpha'
+          'Programming Language :: Python :: 3.10'
+          'Topic :: Environment Setup',
+      ],
+      keywords='onboardme onboarding desktop-setup',
+      version='0.1',
       url='http://github.com/jessebot/onboardme',
       author='Jesse Hitch',
       author_email='jessebot@linux.com',
       license='GPL version 3 or later',
       packages=['onboardme'],
-      install_requires=[
-          'wget',
-          'PyYAML',
-          'rich',
-          'click'
-      ],
+      install_requires=['wget', 'GitPython', 'PyYAML', 'rich', 'click'],
+      scripts=['bin/onboardme'],
       include_package_data=True,
       zip_safe=False)
