3b4280c5e4d987902ee4cc8bc684d5e41ca7fcb4
Jesse Hitch <jessebot@linux.com>
2022-10-26 11:56:00 +0200

    ticking up version of pip package

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(name='onboardme',
                    lic_class],
       python_requires='>3.10',
       keywords='onboardme, onboarding, desktop-setup, setuptools, development',
-      version='0.13.10_a14',
+      version='0.13.10_a15',
       project_urls={
           'Documentation': 'https://jessebot.github.io/onboardme/onboardme',
           'Source': 'http://github.com/jessebot/onboardme',
