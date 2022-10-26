672fefab2bdf6b360b0d45ff0b3bd1bad7bee840
cloudymax <admin@cloudydev.net>
2022-10-26 15:58:46 +0200

    adds support for spacechalk in hyper terminal

--- dot_files/.hyper.js
+++ dot_files/.hyper.js
@@ -140,7 +140,7 @@ module.exports = {
     //   `hyperpower`
     //   `@company/project`
     //   `project#1.0.1`
-    plugins: ["hyper-whimsy", "hyperterm-tabs", "hyper-quit", "hyperinator"],
+    plugins: ["hyperterm-tabs", "hyper-quit", "hyperinator"],
     // in development, you can create a directory under
     // `~/.hyper_plugins/local/` and include it here
     // to load it and avoid it being `npm install`ed
