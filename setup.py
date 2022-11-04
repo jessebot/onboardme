9a72730f7dc7562d2e199284e4fcb3489efb70fb
JesseBot <jessebot@linux.com>
2022-11-04 17:34:10 +0100

    Fix Issue #45: neovim packer install was not returning (#49)
    * fixing the neovim setup, finally
    
    * vim never returns when plugclean is passed in, so fixed that too
    
    * ticking version up

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(name='onboardme',
                    lic_class],
       python_requires='>3.10',
       keywords='onboardme, onboarding, desktop-setup, setuptools, development',
-      version='0.14.0_a',
+      version='0.14.8',
       project_urls={
           'Documentation': 'https://jessebot.github.io/onboardme/onboardme',
           'Source': 'http://github.com/jessebot/onboardme',
