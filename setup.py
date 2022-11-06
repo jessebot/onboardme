a0a97fad6586c771bd87ce3a9aecf603b394e06f
Jesse Hitch <jessebot@linux.com>
2022-11-06 22:41:27 +0100

    ticking up version

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(name='onboardme',
                    lic_class],
       python_requires='>3.10',
       keywords='onboardme, onboarding, desktop-setup, setuptools, development',
-      version='0.14.13_a10',
+      version='0.14.13',
       project_urls={
           'Documentation': 'https://jessebot.github.io/onboardme/onboardme',
           'Source': 'http://github.com/jessebot/onboardme',
