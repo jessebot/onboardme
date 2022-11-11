fa2dcb2970e6ecf0fcde4ab81fd86e0f84a338b2
Jesse Hitch <jessebot@linux.com>
2022-11-11 13:29:02 +0100

    getting basic pyproject.toml ready and adidng some docs to go with it

--- docs/python/packaging.md
+++ docs/python/packaging.md
@@ -9,19 +9,37 @@ permalink: /python/packaging
 
 # How to package your Python programs
 
-# setup.py
-This method is largely being deprecated, but you can read about it [here](/python/packaging/setuppy).
+## setup.py
+This method is largely being deprecated, but you can read about it [here][0].
 
-# myproject.toml
+## myproject.toml
 I have only just begun to do this, which is sad, because apparently this
-[became a standard]() back in 2016 :facepalm:
+became [a standard][1] back in 2016 :facepalm:
 
 This stackoverflow question was helpful to get started though:
-[What is pyproject.toml file for?](https://stackoverflow.com/questions/62983756/what-is-pyproject-toml-file-for)
+[What is pyproject.toml file for?][2]
 
-# What about brew?
+### Poetry
+
+Here's some basics about poetry.
+```bash
+# init a poetry project in a directory with a project already - interactive
+poetry init
+
+# builds the project for publishing
+poetry build
+
+# publish to pypi
+poetry publish
+```
+
+## What about brew?
 Well, that's a rabbit hole that I haven't had a chance to go down yet, but you
 probably want to start here:
-= [Adding Software To Homebrew](https://docs.brew.sh/Adding-Software-to-Homebrew#formulae)
+- [Adding Software To Homebrew](https://docs.brew.sh/Adding-Software-to-Homebrew#formulae)
 - [https://docs.brew.sh/Formula-Cookbook](https://docs.brew.sh/Formula-Cookbook)
 - [https://docs.brew.sh/Python-for-Formula-Authors](https://docs.brew.sh/Python-for-Formula-Authors)
+
+[0]: https://jessebot.github.io/onboardme/python/packaging/setuppy "setuppy"
+[1]: https://peps.python.org/pep-0517/ "pep-0517"
+[2]: https://stackoverflow.com/questions/62983756/what-is-pyproject-toml-file-for "What is pyproject.toml file for"
--- setup.py
+++ setup.py
@@ -1,45 +1,4 @@
 from setuptools import setup
 
 
-def readme():
-    """
-    grab and return contents of README.md for use in long description
-    """
-    with open('README.md') as f:
-        return f.read()
-
-
-lic_class = ('License :: OSI Approved :: GNU Affero General Public License v3'
-             ' or later (AGPLv3+)')
-
-setup(name='onboardme',
-      description='An onboarding tool to install dot files and packages',
-      long_description=readme(),
-      long_description_content_type='text/markdown',
-      classifiers=['Development Status :: 3 - Alpha',
-                   'Programming Language :: Python :: 3.10',
-                   'Operating System :: MacOS :: MacOS X',
-                   'Operating System :: POSIX :: Linux',
-                   'Intended Audience :: End Users/Desktop',
-                   'Topic :: System :: Installation/Setup',
-                   lic_class],
-      python_requires='>3.10',
-      keywords='onboardme, onboarding, desktop-setup, setuptools, development',
-      version='0.14.16',
-      project_urls={
-          'Documentation': 'https://jessebot.github.io/onboardme/onboardme',
-          'Source': 'http://github.com/jessebot/onboardme',
-          'Tracker': 'http://github.com/jessebot/onboardme/issues'},
-      author='jessebot',
-      author_email='jessebot@linux.com',
-      license='GPL version 3 or later',
-      packages=['onboardme'],
-      install_requires=['wget', 'GitPython', 'PyYAML', 'rich', 'click'],
-      data_files=[('onboardme/config',
-                   ['onboardme/config/onboardme_config.yml',
-                    'onboardme/config/packages.yml',
-                    'onboardme/config/brew/Brewfile_Darwin',
-                    'onboardme/config/brew/Brewfile_devops'])],
-      entry_points={'console_scripts': ['onboardme = onboardme:main']},
-      include_package_data=True,
-      zip_safe=False)
+setup(name='onboardme')
