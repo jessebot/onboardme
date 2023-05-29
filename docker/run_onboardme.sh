107b452c0e4570a783e83f92b218e64f7ec3a549
Jesse Hitch <jessebot@linux.com>
2023-05-29 19:25:04 +0200

    change from backticks to string for linux/amd64 run script

--- docker/run_onboardme.sh
+++ docker/run_onboardme.sh
@@ -6,7 +6,7 @@ if [ ! -z $DEVOPS ]; then EXTRA_GROUPS+=" -g devops"; fi
 if [ ! -z $MAIL ]; then EXTRA_GROUPS+=" -g mail"; fi
 if [ ! -z $MUSIC ]; then EXTRA_GROUPS+=" -g music"; fi
 
-obm_cmd=`onboardme --no_upgrade --log_level debug$EXTRA_GROUPS`
+obm_cmd="onboardme --no_upgrade --log_level debug$EXTRA_GROUPS"
 echo "running onboardme command now: $obm_cmd"
 eval $obm_cmd
 echo "finished running onboardme"
