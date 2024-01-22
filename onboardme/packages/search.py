2f99baf81d256ada5cc9d764d013051c947dce57
jessebot <jessebot@linux.com>
2024-01-22 11:45:56 +0100

    ignore if no search possible for pip

--- onboardme/packages/search.py
+++ onboardme/packages/search.py
@@ -5,7 +5,10 @@ def search_for_package(cfg: dict, package_manager: str|list = None):
     """
     Search for package using package manager's search function.
     Takes packages config dict and optional package_manager (str or list) to search
+    returns output of search command
+
     If no package_manager is passed in, we search every package_manager.commands.search
+    returns outputs of search commands as dictionary like {'brew': output}
     """
 
     if package_manager:
@@ -14,5 +17,6 @@ def search_for_package(cfg: dict, package_manager: str|list = None):
     else:
         results = {}
         for package_manager, metadata in cfg.items():
-            search_cmd = cfg[package_manager]['commands']['search']
-            results[package_manager] = subproc([search_cmd])
+            search_cmd = cfg[package_manager]['commands'].get('search', None)
+            if search_cmd:
+                results[package_manager] = subproc([search_cmd])
