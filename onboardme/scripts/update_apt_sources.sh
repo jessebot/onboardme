5e44c62bf9410e18dc5951d41ff2ecbc5fd15a1e
jessebot <jessebot@linux.com>
2024-01-24 11:34:14 +0100

    adding more repos for apt for lutris

--- onboardme/scripts/update_apt_sources.sh
+++ onboardme/scripts/update_apt_sources.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
-# enables contribution and non-free apt package manager sources
+# OS_VERSION=$1
+OS_VERSION="12"
 
+# enables contribution and non-free apt package manager sources
 # append "contrib non-free" to lines that end with "bookworm main"
 sed -i 's/bookworm main$/bookworm main contrib non-free/g' /etc/apt/sources.list
+
+
+# add lutris repos
+# see: https://software.opensuse.org/download.html?project=home%3Astrycore&package=lutris
+# also see: https://lutris.net/downloads
+echo "deb [signed-by=/etc/apt/keyrings/lutris.gpg] https://download.opensuse.org/repositories/home:/strycore/$OS_VERSION/ ./" | sudo tee /etc/apt/sources.list.d/lutris.list > /dev/null
+wget -q -O- https://download.opensuse.org/repositories/home:/strycore/Debian_$OS_VERSION/Release.key | gpg --dearmor | sudo tee /etc/apt/keyrings/lutris.gpg > /dev/null
+sudo apt update
